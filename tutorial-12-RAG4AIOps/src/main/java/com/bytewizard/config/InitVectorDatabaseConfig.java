9e508f0020ff2909b075ad28ea9672ffa7aac1a4
ZaneX <1393895459@qq.com>
2026-03-02 16:28:27 +0800

    update RAG demo system: add duplicate check before adding to vector store

--- tutorial-12-RAG4AIOps/src/main/java/com/bytewizard/config/InitVectorDatabaseConfig.java
+++ tutorial-12-RAG4AIOps/src/main/java/com/bytewizard/config/InitVectorDatabaseConfig.java
@@ -1,5 +1,6 @@
 package com.bytewizard.config;
 
+import cn.hutool.crypto.SecureUtil;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.TextReader;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.nio.charset.Charset;
 import java.util.List;
@@ -18,6 +20,9 @@ public class InitVectorDatabaseConfig {
 
     @Autowired
     private VectorStore vectorStore;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Value("classpath:document/ops.txt")
     private Resource opsFile;
@@ -32,7 +37,23 @@ public class InitVectorDatabaseConfig {
         List<Document> list = new TokenTextSplitter().transform(textReader.read());
 
         // 3 写入向量数据库redis stack
-        vectorStore.add(list);
+        // vectorStore.add(list);
+
+        // 解决第三部向量数据重复问题, 使用redis setnx命令
+        // 3 去重版
+        String metaData = (String) textReader.getCustomMetadata().get("source");
+        String textHash = SecureUtil.md5(metaData);
+        String redisKey = "vector-xxx" + textHash;
+
+        // 判断是否存入过
+        Boolean retFlag = redisTemplate.opsForValue().setIfAbsent(redisKey, "1");
+
+        if (Boolean.TRUE.equals(retFlag)) {
+            // 健不存在, 首次插入
+            vectorStore.add(list);
+        } else {
+            System.out.println("向量初始化数据已经加载");
+        }
 
     }
 
