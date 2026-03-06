0107bfde4629afe0e7a4ef0fae42047ee68d63e8
ZaneX <1393895459@qq.com>
2026-03-06 20:01:38 +0800

    websocket handshake interpretor added

---
+++ AIWorkHelper/src/main/java/com/bytewizard/websocket/ChatWebSocketHandler.java
@@ -0,0 +1,43 @@
+package com.bytewizard.websocket;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+/**
+ * WebSocket聊天处理器
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatWebSocketHandler implements WebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+}
